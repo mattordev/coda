c81708722dd7db5bb1d6749f9c54135ac4710301
mr232432 <mr232432@falmouth.ac.uk>
2022-08-30 11:43:51 +0100

    Added openAI to the response function
    Added the proper code to the response function. Needs more work

--- main.py
+++ main.py
@@ -6,94 +6,106 @@ from turtle import clear
 import speech_recognition as sr
 import pyttsx3 as tts
 
+import openai
+from api_secrets import API_KEY
+openai.api_key = API_KEY
 
 
-Clear = lambda: os.system('cls')
-command_file_location = os.getcwd()+ "\Commands\\"
+def Clear(): return os.system('cls')
+
+
+command_file_location = os.getcwd() + "\Commands\\"
 sys.path.append(command_file_location)
 
-commands = {} # ALL COMMANDS TO BE USED BY OPERATOR
+commands = {}  # ALL COMMANDS TO BE USED BY OPERATOR
 for cmdFile in os.listdir(command_file_location):
-    name = os.fsdecode(cmdFile)       
+    name = os.fsdecode(cmdFile)
     if(name.endswith(".py")):
-        module = SourceFileLoader(cmdFile, command_file_location+cmdFile).load_module()
+        module = SourceFileLoader(
+            cmdFile, command_file_location+cmdFile).load_module()
         commands[name.split(".py")[0].lower()] = module
 
 
 ### THIS FUNCTION PROCESSES COMMANDS FROM INPUTS PASSED INTO IT ###
 def on_command_old(msg):
-    if(len(msg) <= 0): return
+    if(len(msg) <= 0):
+        return
 
-    msg = msg.lower().split() # LOWERS AND SEPERATES EACH WORD INTO AN INDEXED ARRAY
+    msg = msg.lower().split()  # LOWERS AND SEPERATES EACH WORD INTO AN INDEXED ARRAY
 
-    cmd = msg[0]    
+    cmd = msg[0]
     if(cmd in commands):
-        args = [] # HOLDS THE PARAMETERS OF A COMMAND
+        args = []  # HOLDS THE PARAMETERS OF A COMMAND
 
-        for i in range(1, len(msg)): # ITERATE THROUGH THE MESSAGE, IGNORING THE COMMAND INDEX
+        for i in range(1, len(msg)):  # ITERATE THROUGH THE MESSAGE, IGNORING THE COMMAND INDEX
             args.append(msg[i])
-        
-        if(not commands[cmd].run(args)): # CALLS THE FUNCTION AND SIMULTANEOUSLY CHECKS TO SEE IF IT FAILED
+
+        # CALLS THE FUNCTION AND SIMULTANEOUSLY CHECKS TO SEE IF IT FAILED
+        if(not commands[cmd].run(args)):
             Clear()
             print("Command failed to execute... Please try again!")
-            return                
+            return
     else:
         Clear()
         print("Command does not exist... Please try again!")
 
+
 def on_command(msg):
     # Guard clause to stop empty messages.
-    if (len(msg) <= 0): return 
-    
+    if (len(msg) <= 0):
+        return
+
     # Make the message lowercase, split it into an array.
     msg = msg.lower().split()
-    
+
     for i in range(1, len(msg)):
         if (msg[i] in commands):
-            args = [] # This holds any params of the command
+            args = []  # This holds any params of the command
             cmd = msg[i]
             if (not commands[cmd].run(args)):
                 Clear()
                 print("Command failed to execute... Please try again!")
         else:
-            Clear()
             #print("Command does not exist... Please try again!")
-            print ("Checking rest of messasge for command")
+            print("Checking rest of messasge for command")
             Clear()
+
 
 def display_message(message):
     print(message)
 
+
 def speak_response(response):
+    # Generate the response using OpenAI
+    prompt = message
+    ai_response = openai.Completion.create(
+        engine="text-davinci-002", prompt=prompt, max_tokens=12)
     # Initialize text-to-speech
     speaker = tts.init
     # Set how fast it will talk.
     speaker.setProperty('rate', 200)
     speaker.say(response)
-
+    speaker.say(ai_response)
 
 
 ### MAIN ###
 ## This loops, asking users to input a command here after saying the keyword. ##
-
 wakeword = "coda"
 
 while True:
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        audio = r.adjust_for_ambient_noise(source)   
+        audio = r.adjust_for_ambient_noise(source)
         print('Ready to accept commands')
         audio = r.listen(source)
-#find out whats been said(Google Speech Recognition)
+# find out whats been said(Google Speech Recognition)
     try:
         speech = (r.recognize_google(audio))
         message = (speech.lower())
-        
-        
-        if (wakeword) in message:
-           display_message(message)
-           on_command(str(message)) 
 
+        if (wakeword) in message:
+            display_message(message)
+            on_command(str(message))
 
     # exceptions
     except sr.UnknownValueError:
