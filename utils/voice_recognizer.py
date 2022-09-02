b2ee61a2c765120cade8058a16a98968a97ca548
mr232432 <mr232432@falmouth.ac.uk>
2022-09-02 10:57:59 +0100

    feat: added checks for using offline voice processing
    Will now attempt to use google's voice API for when there is a connection, and pocketsphinx for when there isn't. Although, this isn't currently working as mentioned in #1 it does stop the program from crashing if there's no connection

--- utils/voice_recognizer.py
+++ utils/voice_recognizer.py
@@ -1,7 +1,9 @@
 # TODO: Implement PocketSphinx for offline voice recognition and use google's API for when there is a connection
 
 import speech_recognition as sr
+import pocketsphinx5 as ps5
 import utils.speak_response as speak
+import commands.connected as connected
 
 
 def run(wakeword):
@@ -12,13 +14,18 @@ def run(wakeword):
             print('Ready to accept commands')
             try:
                 audio = r.listen(source, timeout=0.25)
-
-                speech = (r.recognize_google(audio))
+                if (connected.run() == True):
+                    print ("LOG: Using online voice API")
+                    speech = (r.recognize_google(audio))
+                else:
+                    print ("LOG: Using offline voice API")
+                    speech = (r.recognize_sphinx(audio_data=audio))
+                
                 message = (speech.lower())
                 display_message(message)
 
                 if not wakeword.isdisjoint(message.lower().split()):
-                    on_command(str(message))
+                    on_command(str(message)) # Errors because code for this is still in main
 
             # exceptions
             except sr.UnknownValueError:
