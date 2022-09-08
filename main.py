a33bfdc1575e71396d8f4713682283ecbc0511f0
mr232432 <mr232432@falmouth.ac.uk>
2022-09-08 15:26:04 +0100

    feat/refactor: added wakeword JSON saving and started work on saving commands to JSON
    wakewords are now saved to JSON #8
    commands need some extra work, they currently throw an error when running due to how the dictionary is setup #7

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Imports
 import os
 import sys
+import json
 from importlib.machinery import SourceFileLoader
 import utils.voice_recognizer as voice_recognizer
 
@@ -19,10 +20,25 @@ def setup_commands():
                 cmdFile, command_file_location+cmdFile).load_module()
             commands[name.split(".py")[0].lower()] = module
             print(commands)
+            # after setup we save the commands to a JSON file
+            #save_commands()
+            
+# def save_commands(): 
+#     jsonCommands = json.dumps(commands)
+#     jsonCommandsFile = open("commands.json", "w")
+#     jsonCommandsFile.write(jsonCommands)
+#     jsonCommandsFile.close()
+    
+def save_wakewords(wakewords):
+    jsonWakewords = json.dumps(wakewords)
+    jsonWakewordsFile = open("wakewords.json", "w")
+    jsonWakewordsFile.write(jsonWakewords)
+    jsonWakewordsFile.close()
 
 ### MAIN ###
-wakeword = {"coda", "kodak", "coder", "skoda", "powder"}
+wakewords = ["coda", "kodak", "coder", "skoda", "powder"]
 
 # Calls the voice recognizer to listen to the microphone
 setup_commands()
-voice_recognizer.run(wakeword, commands)
+save_wakewords(wakewords)
+voice_recognizer.run(wakewords, commands, type='normal')
