dca738016c8549eca29ce281010452ebac300ec7
Matthew Roberts <mr232432@falmouth.ac.uk>
2023-06-19 02:15:50 +0100

    feat: added more wakewords to the list

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import utils.voice_recognizer as voice_recognizer
 
 commands = {}  # ALL COMMANDS TO BE USED BY OPERATOR
 
+
 def setup_commands():
     command_file_location = os.getcwd() + "\commands\\"
     sys.path.append(command_file_location)
@@ -18,18 +19,20 @@ def setup_commands():
         name = os.fsdecode(cmdFile)
         if name.endswith(".py"):
             command_path = os.path.join(command_file_location, cmdFile)
-            module = SourceFileLoader(name.split(".py")[0].lower(), command_path).load_module()
+            module = SourceFileLoader(name.split(
+                ".py")[0].lower(), command_path).load_module()
             commands[name.split(".py")[0].lower()] = module
 
     # Save the commands to a JSON file after the setup is complete
     save_commands()
-            
+
+
 def save_commands():
     print("----------------------------------------", flush=True)
     print("PRINTING FOUND COMMANDS:")
     print(commands)
     print("----------------------------------------")
-    
+
     serialized_commands = {}
     for cmd_name, cmd_module in commands.items():
         serialized_commands[cmd_name] = {
@@ -39,17 +42,19 @@ def save_commands():
 
     with open("commands.json", "w") as outfile:
         json.dump(serialized_commands, outfile)
-    
+
+
 def save_wakewords(wakewords):
     jsonWakewords = json.dumps(wakewords)
     jsonWakewordsFile = open("wakewords.json", "w")
     jsonWakewordsFile.write(jsonWakewords)
     jsonWakewordsFile.close()
 
+
 ### MAIN ###
-wakewords = ["coda", "kodak", "coder", "skoda", "powder"]
+wakewords = ["coda", "kodak", "coder", "skoda", "powder", "kodi", "system"]
 
 # Calls the voice recognizer to listen to the microphone
 setup_commands()
 save_wakewords(wakewords)
-voice_recognizer.run(wakewords,commands, type='normal')
+voice_recognizer.run(wakewords, commands, type='normal')
