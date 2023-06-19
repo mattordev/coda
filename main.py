598e4fb1c591e96036a64d6049514938946fd285
mr232432 <mr232432@falmouth.ac.uk>
2023-06-19 18:24:51 +0100

    feat: version checking and online version checking

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import json
 from importlib.machinery import SourceFileLoader
 import utils.voice_recognizer as voice_recognizer
 
+import semantic_version
+import requests
+
 commands = {}  # ALL COMMANDS TO BE USED BY OPERATOR
+version_url = 'https://raw.githubusercontent.com/mattordev/coda/main/version.json'
+
 
 
 def setup_commands():
@@ -49,6 +54,36 @@ def save_wakewords(wakewords):
     jsonWakewordsFile = open("wakewords.json", "w")
     jsonWakewordsFile.write(jsonWakewords)
     jsonWakewordsFile.close()
+    
+import json
+import requests
+import semantic_version
+
+
+
+def check_update_available(version_url):
+    try:
+        with open("version.json", "r") as json_file:
+            json_data = json.load(json_file)
+            saved_version = semantic_version.Version(json_data['version'])
+    except (IOError, KeyError, json.JSONDecodeError):
+        # Create new version.json with initial version number
+        saved_version = semantic_version.Version('0.0.0')
+        with open("version.json", "w") as json_file:
+            json.dump({'version': str(saved_version)}, json_file)
+
+    try:
+        version_response = requests.get(version_url)
+        if version_response.status_code == 200:
+            latest_version = version_response.text.strip()
+            print("New version has been dected! Please update!")
+            return semantic_version.Version(latest_version) > saved_version
+    except requests.RequestException:
+        pass
+
+    return False
+
+
 
 
 ### MAIN ###
@@ -57,4 +92,5 @@ wakewords = ["coda", "kodak", "coder", "skoda", "powder", "kodi", "system"]
 # Calls the voice recognizer to listen to the microphone
 setup_commands()
 save_wakewords(wakewords)
+check_update_available(version_url)
 voice_recognizer.run(wakewords, commands, type='normal')
