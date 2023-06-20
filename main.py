20ea4132c2d34d428d71efbd82a3a475a682bb5c
mr232432 <mr232432@falmouth.ac.uk>
2023-06-20 01:20:21 +0100

    feat: attempting to get json command saving

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import json
+import importlib
 from importlib.machinery import SourceFileLoader
 import utils.voice_recognizer as voice_recognizer
 
@@ -10,8 +11,6 @@ import requests
 
 commands = {}  # ALL COMMANDS TO BE USED BY OPERATOR
 version_url = 'https://raw.githubusercontent.com/mattordev/coda/main/version.json'
-
-
 
 def setup_commands():
     command_file_location = os.getcwd() + "\commands\\"
@@ -31,7 +30,6 @@ def setup_commands():
     # Save the commands to a JSON file after the setup is complete
     save_commands()
 
-
 def save_commands():
     print("----------------------------------------", flush=True)
     print("PRINTING FOUND COMMANDS:")
@@ -48,16 +46,11 @@ def save_commands():
     with open("commands.json", "w") as outfile:
         json.dump(serialized_commands, outfile)
 
-
 def save_wakewords(wakewords):
     jsonWakewords = json.dumps(wakewords)
     jsonWakewordsFile = open("wakewords.json", "w")
     jsonWakewordsFile.write(jsonWakewords)
     jsonWakewordsFile.close()
-    
-import json
-import requests
-import semantic_version
 
 def check_update_available(version_url):
     try:
@@ -69,17 +62,37 @@ def check_update_available(version_url):
         if version_response.status_code == 200:
             response_json = version_response.json()
             latest_version = response_json['version']
-            return latest_version != saved_version
+            latest_semantic_version = semantic_version.Version(latest_version)
+            return latest_semantic_version > saved_version  # Compare the versions
+
     except (IOError, KeyError, requests.RequestException, ValueError):
         pass
 
     return False
 
+def run_first_time_setup():
+    print("Commands file not found. Assuming first time setup...")
+    setup_commands()
+    save_wakewords(wakewords)
+
 ### MAIN ###
 wakewords = ["coda", "kodak", "coder", "skoda", "powder", "kodi", "system"]
 
-# Calls the voice recognizer to listen to the microphone
-setup_commands()
+if check_update_available(version_url):
+    # if there's an update available, re-find the commands
+    setup_commands()
+    print("Setting up commands as a new version was found...")
+else:
+    try:
+        # load the commands from JSON.
+        with open("commands.json", "r") as command_file:
+            print("Loading commands from JSON")
+
+            print(commands)
+    except FileNotFoundError:
+        run_first_time_setup()
+
+#run_first_time_setup()
 save_wakewords(wakewords)
-check_update_available(version_url)
+# Calls the voice recognizer to listen to the microphone
 voice_recognizer.run(wakewords, commands, type='normal')
