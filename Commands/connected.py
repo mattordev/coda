fff96a43be953ae1418e9f11acb50635799ca0c6
Matthew Roberts <mr232432@falmouth.ac.uk>
2023-06-22 11:15:46 +0100

    chore: moved is_connected outside of run function for use elsewhere

--- Commands/connected.py
+++ Commands/connected.py
@@ -6,19 +6,20 @@ import os
 import utils.speak_response as speak
 
 
-def run(args):
+def is_connected():
     REMOTE_SERVER = "www.google.com"
-    
-    def is_connected():
-        try:
-            host = socket.gethostbyname(REMOTE_SERVER)
-            s = socket.create_connection((host, 80), 2)
-            return True
-        except:
-            pass
-        return False
 
-    if(is_connected()):
+    try:
+        host = socket.gethostbyname(REMOTE_SERVER)
+        s = socket.create_connection((host, 80), 2)
+        return True
+    except:
+        pass
+    return False
+
+
+def run(args):
+    if (is_connected()):
         response = random.choice(
             ['We are connected', 'There is an established data connection', 'We are online', 'We are up and running, with consistent data streams'])
         print(response)
@@ -26,4 +27,3 @@ def run(args):
         return True
     else:
         return False
-
