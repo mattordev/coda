1795650d3989bb3481fbb9a577b50d998193d06c
Matthew Roberts <mr232432@falmouth.ac.uk>
2023-06-22 11:16:28 +0100

    feat: voice recog now checks for internet connection
    this decides which package it uses, elevenalabs, or pyttsx3

--- utils/speak_response.py
+++ utils/speak_response.py
@@ -1,13 +1,29 @@
 import pyttsx3 as tts
+from elevenlabs import generate, play, stream, set_api_key
+import commands.connected as connected
+
+elevenLabsAPIKey = 'abe3e7684e46c45769406e9f76f5f936'
+
+user = set_api_key(elevenLabsAPIKey)
 
 
 def speak_response(response):
-    
-    # Initialize text-to-speech
-    speaker = tts.init()
-    voices = speaker.getProperty('voices')
-    # Set how fast it will talk.
-    speaker.setProperty("rate", 175)
-    speaker.setProperty('voice', voices[0].id)
-    speaker.say(response)
-    speaker.runAndWait()
+
+    if connected.is_connected():
+        print("Using Eleven labs for speech")
+        audio = generate(
+            text=response,
+            voice="Rachel",
+            model="eleven_monolingual_v1"
+        )
+        play(audio)
+    else:
+        print("using pyttsx3 for speech")
+        # Initialize text-to-speech
+        speaker = tts.init()
+        voices = speaker.getProperty('voices')
+        # Set how fast it will talk.
+        speaker.setProperty("rate", 175)
+        speaker.setProperty('voice', voices[0].id)
+        speaker.say(response)
+        speaker.runAndWait()
