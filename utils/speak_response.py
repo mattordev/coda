0923b939b2411a45c829a01a6bc16924ee43ba90
Matthew Roberts <mr232432@falmouth.ac.uk>
2023-06-23 10:56:53 +0100

    feat: elevenlabs voice synthesis

--- utils/speak_response.py
+++ utils/speak_response.py
@@ -1,5 +1,5 @@
 import pyttsx3 as tts
-from elevenlabs import generate, play, set_api_key
+from elevenlabs import voices, generate, play, set_api_key
 import commands.connected as connected
 
 elevenLabsAPIKey = 'abe3e7684e46c45769406e9f76f5f936'
@@ -10,20 +10,27 @@ user = set_api_key(elevenLabsAPIKey)
 def speak_response(response):
 
     if connected.is_connected():
-        print("Using Eleven labs for speech")
-        audio = generate(
-            text=response,
-            voice="Rachel",
-            model="eleven_monolingual_v1"
-        )
-        play(audio)
+        try:
+            print("Using Eleven labs for speech")
+            audio = generate(
+                text=response,
+                voice="Bella",
+                model="eleven_monolingual_v1"
+            )
+            play(audio)
+        except:
+            use_pyttsxs(response)
     else:
-        print("using pyttsx3 for speech")
-        # Initialize text-to-speech
-        speaker = tts.init()
-        voices = speaker.getProperty('voices')
-        # Set how fast it will talk.
-        speaker.setProperty("rate", 175)
-        speaker.setProperty('voice', voices[0].id)
-        speaker.say(response)
-        speaker.runAndWait()
+        use_pyttsxs(response)
+
+
+def use_pyttsxs(message):
+    print("using pyttsx3 for speech as a fallback")
+    # Initialize text-to-speech
+    speaker = tts.init()
+    voices = speaker.getProperty('voices')
+    # Set how fast it will talk.
+    speaker.setProperty("rate", 175)
+    speaker.setProperty('voice', voices[0].id)
+    speaker.say(message)
+    speaker.runAndWait()
