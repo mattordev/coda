f06b9bf0615dd44a6413dc9569c09a21e03e6f9d
Matthew <mrobertscgd@gmail.com>
2024-03-14 02:46:07 +0000

    feat: started work on update setup

--- utils/update_manager.py
+++ utils/update_manager.py
@@ -1,6 +1,10 @@
 import requests
 from tqdm import tqdm
 import zipfile
+import os
+import shutil
+import json
+import semantic_version
 
 # Updates CODA to the latest version, if available, directly from the GitHub repository. Only runs manually OR if a new version is detected
 # in the start up process.
@@ -23,9 +27,25 @@ def extract_download():
         zip_ref.extractall("coda")
 
 
+# needs elevated permissions to move files: https://stackoverflow.com/a/19719292
 def setup_updated_program():
     # https://stackoverflow.com/a/8858026
     # Sort out file structure. main files are in coda/coda-main and need to be moved out
-    # Move the old coda folder to a backup location
+    os.rename('coda/coda-main', 'coda-new')
+    # Move the new coda folder to the correct location
+    os.replace('coda-new', 'coda')
+    # Move the old coda folder to a backup location, give it a the version number.
+    shutil.move('coda', 'coda-backup')
+    # read current version from version.json
+    with open("version.json", "r") as json_file:
+        json_data = json.load(json_file)
+        saved_version = semantic_version.Version(json_data['version'])
+        json_file.close()
+    os.rename('coda-backup', 'coda-' + saved_version)
     # Move the new coda folder to the correct location
     pass
+
+
+update_program()
+extract_download()
+setup_updated_program()
