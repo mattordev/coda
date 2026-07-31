5d5fc276af52416490c510d9e78e00a83226f38e
Matthew <mrobertscgd@gmail.com>
2026-07-31 13:01:13 +0100

    feat: time command work

--- Commands/time.py
+++ Commands/time.py
@@ -2,20 +2,41 @@ from ai.intents import Intent, IntentParameter, IntentRequest
 
 import utils.speak_response as speak
 
+import datetime
+
 
 INTENT = Intent(
     name="time",
-    description="Ask CODA to report the time",
+    description="Ask CODA to report the time or date.",
     aliases=(
-        "time",
         "current time",
         "what's the time",
         "tell me the time",
         "what time is it",
         "what's the time right now",
+        "current date",
+        "what's the date",
+        "what date is it",
+        "what's the date right now"
     ),
 )
 
+def get_date_time() -> str:
+    formatted_time = datetime.datetime.now().strftime("%H:%M:%p")
+    formatted_date = datetime.datetime.now().strftime("%d/%m/%Y")
+    return {
+        "time": formatted_time,
+        "date": formatted_date
+    }
+
 def run(request: IntentRequest) -> bool:
     """Return the current time."""
+    
+    if ("time" in request.message):
+        print(f"The current time is {get_date_time["time"]}.")
+        speak.speak_response(f"The time is currently {get_date_time["time"]}.")
+    elif("date" in request.message):
+        print(f"The current date is {get_date_time["date"]}.")
+        speak.speak_response(f"The current date is {get_date_time["date"]}.")
+        
     return True
